dcd45997cfb24ca913e98b288704e78a0f479a96
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-04-14 23:52:13 +0200

    03: Make technician parse orders

--- 03_rabbitmq/technician.py
+++ 03_rabbitmq/technician.py
@@ -3,6 +3,7 @@
 import pika
 import sys
 import time
+import random
 
 import common
 from common import errprint
@@ -10,22 +11,26 @@ from common import errprint
 
 def make_skill_callback(skill):
     def callback(ch, method, properties, body):
-        bodystr = body.decode()
-        errprint("handling message {} regarding skill {}".format(bodystr, skill))
+        injury, name = body.decode().split(maxsplit=1)
+
+        if injury != skill:
+            errprint("'{}' handler received unexpected message!".format(skill))
+
+        errprint("performing {} test on {}".format(injury, name))
+        time.sleep(random.uniform(1, 5))
+        errprint("{} test on {} done".format(injury, name))
 
         reply_to = properties.reply_to
-        # TODO correlation id
-        time.sleep(2)
+        response = "{} {} done".format(name, injury)
         ch.basic_publish(exchange=common.EXCHANGE,
                          routing_key=reply_to,
-                         body=bodystr + " DONE")
+                         body=response)
 
     return callback
 
 
 if __name__ == '__main__':
     # TODO listen for INFO messages
-    # TODO send replies to requests
     _connection, channel = common.connect()
 
     for skill in sys.argv[1:3]:
