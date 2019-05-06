3b53af94827d682dee03a93e5e7839d098788152
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-06 05:26:42 +0200

    04: Make server port a CLI argument for the client

--- 04_middleware/bankclient/bankclient.py
+++ 04_middleware/bankclient/bankclient.py
@@ -1,48 +1,73 @@
 #!/usr/bin/env python3
 
-import sys, Ice
+import sys
+import os
+import Ice
 
 Ice.loadSlice('../api/bank.ice')
 import bank
 
-with Ice.initialize(sys.argv) as communicator:
-    # TODO make the client interactive  to switch between accounts
-    # TODO read server port from commandline
-
-    factory_prx = communicator.stringToProxy('accfac/accountfactory:tcp -h localhost -p 10002:udp -h localhost -p 10002')
-    print(factory_prx)
-    factory = bank.AccountFactoryPrx.checkedCast(factory_prx)
-    print(factory)
-    income = bank.MoneyAmount(100020, bank.Currency.PLN)
-    result = factory.createAccount("w", "g", "1234", income)
-    print(result)
-    key = result.key
-
-    if result.isPremium:
-        print("Casting to premium account")
-        account = bank.PremiumAccountPrx.checkedCast(result.account)
-
-        print("requesting loan")
-        
-        loanValue = bank.MoneyAmount(3000, bank.Currency.GBP)
-        offer = account.requestLoan(loanValue, 15, {'key': key})
-        print(offer)
-    else:
-        print("standard account")
-        account = result.account
-
-    result2 = factory.createAccount("wewe", "g", "12345", income)
-    print(result2)
-    key2 = result2.key
-    account2 = result2.account
+DEFAULT_PORT = 10000
+port = DEFAULT_PORT
 
 
-    # proxy = communicator.stringToProxy('standard/somepesel:tcp -h localhost -p 10002:udp -h localhost -p 10002')
-    # print('got proxy')
-    # account = Bank.AccountPrx.checkedCast(proxy)
-    # print('got account')
+def printHelp():
+    print("Available commands:")
+    print("register <first name> <last name> <pesel> <monthly income>")
+    print("login <pesel> <key>")
+    print("loan <value> <currency> <period in months>")
 
-    # print("getting balance")
-    # balance= account.getBalance({'key': key + "badkey"})
-    # print(balance)
-    # print(balance.minorUnitAmount)
+
+def getProxy(communicator, name, category):
+    return communicator.stringToProxy('{}/{}:tcp -h localhost -p {port}:udp -h localhost -p {port}'.format(
+                                      category, name, port=port))
+
+
+if __name__ == '__main__':
+    print("main")
+    try:
+        port = int(sys.argv[1])
+    except Exception as e:
+        print("Defaulting to port number {}".format(DEFAULT_PORT))
+
+
+    with Ice.initialize(sys.argv) as communicator:
+        # TODO make the client interactive  to switch between accounts
+        # TODO read server port from commandline
+
+        factory_prx = getProxy(communicator, "accountfactory", "accountfactory")
+        print(factory_prx)
+        factory = bank.AccountFactoryPrx.checkedCast(factory_prx)
+        print(factory)
+        income = bank.MoneyAmount(100020, bank.Currency.PLN)
+        result = factory.createAccount("w", "g", "1234", income)
+        print(result)
+        key = result.key
+
+        if result.isPremium:
+            print("Casting to premium account")
+            account = bank.PremiumAccountPrx.checkedCast(result.account)
+
+            print("requesting loan")
+
+            loanValue = bank.MoneyAmount(3000, bank.Currency.GBP)
+            offer = account.requestLoan(loanValue, 15, {'key': key})
+            print(offer)
+        else:
+            print("standard account")
+            account = result.account
+
+        result2 = factory.createAccount("wewe", "g", "12345", income)
+        print(result2)
+        key2 = result2.key
+        account2 = result2.account
+
+        # proxy = communicator.stringToProxy('standard/somepesel:tcp -h localhost -p 10002:udp -h localhost -p 10002')
+        # print('got proxy')
+        # account = Bank.AccountPrx.checkedCast(proxy)
+        # print('got account')
+
+        # print("getting balance")
+        # balance= account.getBalance({'key': key + "badkey"})
+        # print(balance)
+        # print(balance.minorUnitAmount)
