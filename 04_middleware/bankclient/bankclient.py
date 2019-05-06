eb5e419a3605424acf9a987cdb8c47a615483c52
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-06 06:45:29 +0200

    04: Request loan

--- 04_middleware/bankclient/bankclient.py
+++ 04_middleware/bankclient/bankclient.py
@@ -19,9 +19,20 @@ class Account:
     def getBalance(self):
         return self.proxy.getBalance({'key': self.key})
 
+    def requestLoan(self, *args):
+        return self.proxy.requestLoan(*args, {'key': self.key})
+
 
 DEFAULT_PORT = 10000
 BASE_CURRENCY = bank.Currency.PLN
+
+CURRENCY = {
+    'PLN': bank.Currency.PLN,
+    'EUR': bank.Currency.EUR,
+    'GBP': bank.Currency.GBP,
+    'USD': bank.Currency.USD,
+    'HRK': bank.Currency.HRK,
+}
 
 port = DEFAULT_PORT
 current_account: Optional[Account] = None
@@ -69,6 +80,8 @@ def formatPrompt():
         return "{pesel}>".format(pesel=current_account.pesel)
 
 
+
+
 def handleRegister(communicator, args):
     global current_account
     ensureArgsLen(args, 4)
@@ -82,11 +95,17 @@ def handleRegister(communicator, args):
     income_obj = bank.MoneyAmount(income * 100, BASE_CURRENCY)
     try:
         result = factory.createAccount(fname, lname, pesel, income_obj)
-        current_account = Account(
-            result.account, pesel, result.key, result.isPremium)
+        category = "premium" if result.isPremium else "standard"
 
-        premium_str = "premium" if result.isPremium else "standard"
-        print("Registered a {} account. Access key: {}".format(premium_str, result.key))
+        if result.isPremium:
+            account_proxy = bank.PremiumAccountPrx.checkedCast(result.account)
+        else:
+            account_proxy = bank.AccountPrx.checkedCast(result.account)
+
+        current_account = Account(
+            account_proxy, pesel, result.key, result.isPremium)
+
+        print("Registered a {} account. Access key: {}".format(category, result.key))
     except bank.AccountExistsException:
         print("Account using this PESEL is already registered!")
 
@@ -109,18 +128,45 @@ def handleLogin(communicator, args):
 
         print("Logged in. Current balance: " + formatBalance(balance))
         current_account = Account(
-            account, pesel, key, category.upper() == 'premium')
+            account, pesel, key, category.lower() == 'premium')
     except Ice.ObjectNotExistException:
         print("Given account does not exist!")
 
 
 def handleBalance(communicator, args):
+    if current_account is None:
+        print("You must log in to check balance!")
+        return
     balance = current_account.getBalance()
     print("Current balance: " + formatBalance(balance))
 
 
 def handleLoan(communicator, args):
-    pass
+    if current_account is None:
+        print("You must log in to request a loan!")
+        return
+
+    ensureArgsLen(args, 3)
+    [value, currency_str, duration] = args
+
+    print(current_account.is_premium)
+    print(current_account.key)
+    if not current_account.is_premium:
+        print("Standard account cannot request a loan!")
+        return
+
+    if currency_str.upper() in CURRENCY:
+        currency = CURRENCY[currency_str.upper()]
+    else:
+        raise ValueError("Unknown currency!")
+
+    value_obj = bank.MoneyAmount(int(float(value) * 100), CURRENCY[currency_str.upper()])
+    result = current_account.requestLoan(value_obj, int(duration))
+
+    print("Loan cost: {:.2f} {} ({:.2f} {})".format(
+        result.foreignCost.minorUnitAmount/ 100, result.foreignCost.currency,
+        result.convertedCost.minorUnitAmount/ 100, result.convertedCost.currency))
+
 
 
 def handleExit(communicator, _args):
@@ -155,7 +201,8 @@ def repl_loop(communicator):
             print("Bad access key!")
         except Exception as error:
             print("Error: ", error)
-
+    def getBalance(self):
+        return self.proxy.getBalance({'key': self.key})
 
 if __name__ == '__main__':
     try:
