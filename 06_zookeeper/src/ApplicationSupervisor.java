af9262bbfc3af5b1bfdd024a5f18c455e41f07f8
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-06-01 18:54:58 +0200

    06: Spawn external process when znode is created

--- 06_zookeeper/src/ApplicationSupervisor.java
+++ 06_zookeeper/src/ApplicationSupervisor.java
@@ -1,5 +1,7 @@
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 
@@ -9,12 +11,16 @@ public class ApplicationSupervisor {
 
     private ZooKeeper zooKeeper;
     private ZooWatcher watcher;
+
+    private String[] toSpawn;
     private Process spawned = null;
 
     private int previousChildCount = 0;
 
-    public ApplicationSupervisor(final String address) throws IOException, KeeperException,
+    public ApplicationSupervisor(final String address, final String[] toSpawn) throws IOException,
+            KeeperException,
             InterruptedException {
+        this.toSpawn = toSpawn;
         watcher = new ZooWatcher();
         watcher.setOnChildrenChanged(path -> {
             try {
@@ -30,7 +36,17 @@ public class ApplicationSupervisor {
 //        System.out.println("/ze");
 //        printSubtree("/ze");
         zooKeeper.getChildren("/", watcher);
-        zooKeeper.getChildren("/z", watcher);
+        zooKeeper.exists("/z", this::onExistsChanged);
+        zooKeeper.getChildren("/z", watcher, (x, y, z, a, b) -> {}, null);
+    }
+
+    // TODO remember to print tree root beforehand
+    public void printSubtree(final String path) throws KeeperException, InterruptedException {
+        for (String child : zooKeeper.getChildren(path, false)) {
+            final String childPath = path + "/" + child;
+            System.out.println(childPath);
+            printSubtree(childPath);
+        }
     }
 
 
@@ -45,8 +61,43 @@ public class ApplicationSupervisor {
         }
     }
 
+
+    private void onExistsChanged(WatchedEvent event) {
+        System.out.println("onExistsChanged for path " + event.getPath());
+        try {
+            final Stat result = zooKeeper.exists(event.getPath(), this::onExistsChanged);
+            if (result == null) {
+                ensureStopped();
+            } else {
+                ensureStarted();
+                zooKeeper.getChildren("/z", watcher, (x, y, z, a, b) -> {}, null);
+            }
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void ensureStopped() {
+        if (spawned != null && spawned.isAlive()) {
+            spawned.destroy();
+        }
+    }
+
+    private void ensureStarted() {
+        if (spawned == null || !spawned.isAlive()) {
+            try {
+                spawned = Runtime.getRuntime().exec(toSpawn);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     private void refreshState() {
         System.out.println("Something changed in namespace root");
+//        if zooKeeper.get
     }
 
 
@@ -57,16 +108,6 @@ public class ApplicationSupervisor {
             System.out.println(String.format("There are %d children of '%s'", count, path));
         }
         previousChildCount = count;
-    }
-
-    // TODO remember to print tree root beforehand
-    private void printSubtree(final String path) throws KeeperException, InterruptedException {
-        for (String child : zooKeeper.getChildren(path, false)) {
-            final String childPath = path + "/" + child;
-            System.out.println(childPath);
-            printSubtree(childPath);
-        }
-
     }
 
 
--- 06_zookeeper/src/ZooApp.java
+++ 06_zookeeper/src/ZooApp.java
@@ -5,17 +5,21 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class ZooApp {
-    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+    public static void main(String[] args) throws IOException, InterruptedException,
+            KeeperException {
         System.out.println("Args: " + Arrays.toString(args));
 
         String addr;
+        String[] toSpawn = new String[]{"gnome-calculator"};
         if (args.length < 1) {
             addr = "localhost:2181";
         } else {
             addr = args[0];
+            toSpawn = Arrays.copyOfRange(args, 1, args.length);
         }
 
-        ApplicationSupervisor as = new ApplicationSupervisor(addr);
+
+        ApplicationSupervisor as = new ApplicationSupervisor(addr, toSpawn);
 
         Thread.sleep(60 * 1000);
     }
